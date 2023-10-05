b4692485e62cb117c474c84862cc3c333005026e
Joao Reis <joao_reiis@hotmail.com>
2023-10-04 23:34:05 -0300

    WalrusCorp

--- index.js
+++ index.js
@@ -1,71 +1,45 @@
-const child = require('child_process');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 const fs = require('fs');
-const express = require('express')
-const app = express()
-const port = 3001
 const util = require('util');
+const child = require('child_process');
 
 const exec = util.promisify(child.exec);
 
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    const auth = {login: 'a', password: 'a'}
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    if (login && password && login === auth.login && password === auth.password) {
+      return next()
+    }
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+});
+
+app.use(express.static('public'));
+
+app.post('/exec', async (req, res) => {
+    const { command } = req.body;
+    try {
+        let resp = await exec(command);
+        res.send(resp);
+    } catch (error) {
+        res.send(error);
+    }
+});
 
 app.get('/scripts', (req, res) => {
-    
-    fs.readdir("./public/scripts", (err, files) => {
-        files.forEach(file => {
-          console.log(file);
-        });
-  res.send(files)
-      });
+    fs.readdir('./public/scripts', (err, files) => {
+        res.send(files);
+    });
+});
 
-})
-
-app.get('/execute/:i', async (req, res) => {
-    var script = req.params.i;
-    var file = fs.readFileSync('./public/scripts/'+script).toString().split('\r\n');
-    var resp = ""
-    res.write(`
-    
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scripts</title>
-    <style>
-    body {
-      background-color:black;
-    }
-    
-    .terminal {
-      width:100%;
-      height:100%;
-      background-color:black;
-      color:#66cc00;
-      font-size:20px;
-    }
-    </style>
-</head>
-<body>
-<textarea class="terminal" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" id="terminal" cols="100" rows="100">
-    `);
-
-    for (let i = 0; i < file.length; i++) {
-        const element = file[i];
-        const result = await exec(element);
-        res.write(result.stdout);
-
-    }
-
-    res.write(`
-    </textarea>
-    </body>
-    </html>
-    `);
-    res.end()
-})
-
-app.use(express.static('public'))
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ public/scripts/teste.py
@@ -0,0 +1,5 @@
+a = 0
+for i in range(9000000):
+    a = a+i
+
+print(a)
